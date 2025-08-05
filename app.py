7303c844a3d1408ef40e3eb43deabbd7727db0d0
Umar Khattab <ru.khattab@gmail.com>
2025-08-04 21:04:41 -0500

    updates

--- app.py
+++ app.py
@@ -1,10 +1,23 @@
 from fastapi import FastAPI,Request
 from pydantic import BaseModel
 from gradio_client import Client
+from fastapi.middleware.cors import CORSMiddleware
 
 app = FastAPI()
 client = Client("UmarKhattab09/AmazonNLPproject",verbose=False)
+origins = [
+    "http://localhost:3000",
+    "https://amazon-echo-nlp-project.vercel.app",
+    "*"  # Only use "*" in development!
+]
 
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,  # Allows your frontend domain
+    allow_credentials=True,
+    allow_methods=["*"],    # Allow all HTTP methods: GET, POST, etc.
+    allow_headers=["*"],    # Allow all headers
+)
 
 @app.get("/")
 def root():
